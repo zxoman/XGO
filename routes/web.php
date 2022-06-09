bb42b06fc6bd6c2ccc4cdae7f7526af9766b9e74
Your Name <you@example.com>
2022-06-09 00:56:35 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('welcome');
+});
+Route::get('/admin', function () {
+    return view('welcome');
+});
+Route::get('/login', function () {
+    return view('welcome');
+});
+Route::get('/sginup', function () {
+    return view('welcome');
+});
+Route::get('/search', function () {
+    return view('welcome');
+});
+Route::get('/prodact', function () {
+    return view('welcome');
+});
+Route::get('/prodact', function () {
+    return view('welcome');
+});
