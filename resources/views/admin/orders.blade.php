c1abba647b3b99bcc29dc00d6499b1dd3380b399
Your Name <you@example.com>
2022-06-13 05:14:10 -0700

    create frontend to orders page

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -1,28 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>orders</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="/css/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="/css/select2.min.css">
+
+    <link rel="stylesheet" type="text/css" href="/css/perfect-scrollbar.css">
+
+    <link rel="stylesheet" type="text/css" href="/css/util.css">
+
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+    <meta name="robots" content="noindex, follow">
 </head>
+
 <body>
-    <h1>Orders</h1>
-    <div class="orders">
-        <div class="order">
-            <h1>Name : </h1>
-            <h3>Ziad Elsayed</h1>
-            <h1>Phone : </h1>
-            <h3>+2010139413801</h3>
-            <h1>prodacts</h1>
-            <div>
-                <a href="#">laptop</a>
-                <a href="#">laptop</a>
-                <a href="#">laptop</a>
-                
+    <div class="limiter">
+        <div class="container-table100">
+            <div class="wrap-table100">
+                <div class="table100">
+                    <table>
+                        <thead>
+                            <tr class="table100-head">
+                                <th class="column1">Date</th>
+                                <th class="column2">Order ID</th>
+                                <th class="column3">Name Prodact</th>
+                                <th class="column4">Price</th>
+                                <th class="column5">Name Client</th>
+                                <th class="column6">Phone</th>
+                                <th class="column6">Address</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="column1">2017-09-29 01:22</td>
+                                <td class="column2">200398</td>
+                                <td class="column3">iPhone X 64Gb Grey</td>
+                                <td class="column4">$999.00</td>
+                                <td class="column5">ziad elsayed</td>
+                                <td class="column6">+201049449</td>
+                                <td class="column7">cairo 2 elneel street</td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+    <script src="/js/select2.min.js"></script>
+
+    <script src="/js/main.js"></script>
+
+
 </body>
+
 </html>
